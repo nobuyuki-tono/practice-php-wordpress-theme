fd057ea15683e24e8b769bb8062528879943b599
Nobu <rockmusic2090@gmail.com>
2020-09-25 14:36:24 -0700

    Created custom header for practice

---
+++ wphierarchy/header-splash.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <div class="notice">
+    <p>Lorem to the sell thisum</p>
+  </div>
--- wphierarchy/index.php
+++ wphierarchy/index.php
@@ -1,4 +1,4 @@
-<?php get_header(); ?>
+<?php get_header('splash'); ?>
 
 <h1>index.php</h1>
 </body>
