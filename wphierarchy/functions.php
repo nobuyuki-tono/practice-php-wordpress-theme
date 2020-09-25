dded0ebe6ebb1ea60fd690f6732246f716d5c19a
Nobu <rockmusic2090@gmail.com>
2020-09-25 14:11:19 -0700

    Added wordpress function add_theme_support in functions.php

--- wphierarchy/functions.php
+++ wphierarchy/functions.php
@@ -1,5 +1,16 @@
 <?php 
 
+// Add Theme Support
+add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnail' );
+add_theme_support( 'post_format', ['aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'] );
 
+add_theme_support( 'html5');
+add_theme_support('automatic-feed-links');
+add_theme_support("custom-background");
+add_theme_support( 'custom-header');
+add_theme_support( 'custom-logo');
+add_theme_support( 'customize-selective-refresh-widgets' );
+add_theme_support( 'starter-content' );
 
 ?>
