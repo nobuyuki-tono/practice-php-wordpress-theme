29422188e797265df2a6878a2ef9ba575bb688fb
Nobu <rockmusic2090@gmail.com>
2020-09-25 14:23:20 -0700

    Added function to load css style

--- wphierarchy/functions.php
+++ wphierarchy/functions.php
@@ -13,4 +13,11 @@ add_theme_support( 'custom-logo');
 add_theme_support( 'customize-selective-refresh-widgets' );
 add_theme_support( 'starter-content' );
 
+// Load in our CSS
+function wphierarchy_enqueue_styles(){
+  
+  wp_enqueue_style( 'main-css', get_stylesheet_directory_uri() . '/style.css', [], time(), 'all' );
+}
+add_action('wp_enqueue_scripts', 'wphierarchy_enqueue_styles')
+
 ?>
