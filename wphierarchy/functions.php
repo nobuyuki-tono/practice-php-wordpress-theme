45854212fa13e1f3365b400feb2c97773ec3fee8
Nobu <rockmusic2090@gmail.com>
2020-09-25 15:04:13 -0700

    Added navigation menu

--- wphierarchy/functions.php
+++ wphierarchy/functions.php
@@ -18,12 +18,12 @@ function wphierarchy_enqueue_styles(){
   
   wp_enqueue_style( 'main-css', get_stylesheet_directory_uri() . '/style.css', [], time(), 'all' );
 }
-add_action('wp_enqueue_scripts', 'wphierarchy_enqueue_styles')
+add_action('wp_enqueue_scripts', 'wphierarchy_enqueue_styles');
 
 
 // Register  Menu Locations
 register_nav_menus( [
-  'main-menu' => esc_html__('Main Menu', 'wpheirarchy')
+  'main-menu' => esc_html__('Main Menu', 'wpheirarchy'),
 ] );
 
 ?>
--- wphierarchy/header.php
+++ wphierarchy/header.php
@@ -8,3 +8,12 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+  <nav id="site-navigation" class="main-navigation" role="navigation">
+    <?php 
+      $args = [
+        'theme_location' => 'main-menu'
+      ]
+      wp_nav_menu( $args ); 
+    ?>
+  </nav>
