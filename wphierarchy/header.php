9cb583def4be84c55e206c3fd7caf64858583b9a
Nobu <rockmusic2090@gmail.com>
2020-09-25 15:24:48 -0700

    Added html tag to make page structure

--- wphierarchy/header.php
+++ wphierarchy/header.php
@@ -9,11 +9,27 @@
 
 <body <?php body_class(); ?>>
 
-  <nav id="site-navigation" class="main-navigation" role="navigation">
-    <?php 
+  <div id="page">
+
+    <a href="#content" class="skip-link screen-reader-text">
+      <?php esc_html_e( "Skip tp the content", 'wphierarchy' );  ?>
+    </a>
+
+    <header id="masthead" class="site-header" role="banner">
+
+      <div class="site-branding">
+        <p class="site-title"><?php bloginfo( 'name' ) ?></p>
+      </div>
+
+      <nav id="site-navigation" class="main-navigation" role="navigation">
+        <?php 
       $args = [
         'theme_location' => 'main-menu'
       ];
       wp_nav_menu( $args ); 
     ?>
-  </nav>
+      </nav>
+    </header>
+
+
+  </div>
